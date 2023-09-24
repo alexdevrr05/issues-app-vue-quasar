6aa28066cd23d0eef24b0f14d8a21baa29b9bc7e
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2023-09-24 10:09:06 -0600

    Initials configs

---
+++ src/issues/pages/IssuePage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <span class="text-h2">Issue: </span>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/issues/pages/ListPage.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="row q-mb-md">
+    <div class="col-12">
+      <span class="text-h2">Github Issues</span>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-xs-12 col-md-4">
+      <!-- TODO: Filtros -->
+      <!-- FilterSelector -->
+    </div>
+
+    <div class="col-xs-12 col-md-8">
+      <!-- TODO: Loader -->
+
+      <!-- TODO: IssueList - Array de IssueCard -->
+    </div>
+
+  </div>
+</template>
+
+<style scoped></style>
